1a20639bff9b2dd1dbe0dc7550e31765d1ea3ecd
DaphneCuadra <dap.cuadra@duocuc.cl>
2024-08-29 12:03:03 -0400

    validaciones

--- src/app/pages/registro/registro.page.ts
+++ src/app/pages/registro/registro.page.ts
@@ -12,10 +12,30 @@ export class RegistroPage implements OnInit {
   //AQUI PODEMOS CREAR VARIABLES
   persona = new FormGroup({
     rut : new FormControl('',[Validators.minLength(9),Validators.maxLength(10),Validators.required,Validators.pattern("[0-9]{7,8}-[0-9kK]{1}")]),
-    nombre : new FormControl('',[Validators.minLength(3),Validators.required, Validators.pattern("[a-z]{3,5}")]),
-    fecha_nacimiento : new FormControl(),
-    genero : new FormControl()
+    nombre : new FormControl('',[Validators.required, Validators.pattern("[a-z]{3,5}")]),
+    fecha_nacimiento : new FormControl('',[Validators.required]),
+    genero : new FormControl('',[Validators.required])
   });
+
+  public alertButtons = [
+    {
+      text: 'Cancel',
+      role: 'cancel',
+      handler: () => {
+        console.log('Alert canceled');
+      },
+    },
+    {
+      text: 'OK',
+      role: 'confirm',
+      handler: () => {
+        console.log('Alert confirmed');
+      },
+    },
+  ];
+
+
+
 
   constructor(private router : Router) { }
 
@@ -30,5 +50,18 @@ export class RegistroPage implements OnInit {
     this.router.navigate(['/login'])
 
   }
+
+  /*public habilitar_boton():boolean{
+    if(this.persona.valid){
+      return false;
+    }
+    return true;
+  }*/
+
+  setResult(ev:any) {
+    console.log(`Dismissed with role: ${ev.detail.role}`);
+  }
+
+  //INVESTIGAR FECHA NACIMIENTO
 
 }
