673258697b9c5f718e72160a89ef7ccf49be01a7
DaphneCuadra <dap.cuadra@duocuc.cl>
2024-09-05 11:16:23 -0400

    agregamos recuperar y error 404

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -19,6 +19,16 @@ const routes: Routes = [
     path: 'registro',
     loadChildren: () => import('./pages/registro/registro.module').then( m => m.RegistroPageModule)
   },
+  {
+    path: 'recuperar',
+    loadChildren: () => import('./pages/recuperar/recuperar.module').then( m => m.RecuperarPageModule)
+  },
+  {
+    /*Esta página de "ERROR" siempre debe ir al FINAL */
+    path: '**',
+    loadChildren: () => import('./pages/error404/error404.module').then( m => m.Error404PageModule)
+  },
+
   
 
 ];
