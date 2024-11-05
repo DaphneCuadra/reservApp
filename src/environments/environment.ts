a9a29e737b0b584fba5fffbed3114165809417c5
alangajardomedina <alan.gajardo@profesor.duoc.cl>
2024-11-05 12:48:21 -0300

    se crea servicio fire

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyAnNnIZmcy92KCEbGKftAukbKY2PpimpbA",
+    authDomain: "reservapp-3f424.firebaseapp.com",
+    projectId: "reservapp-3f424",
+    storageBucket: "reservapp-3f424.firebasestorage.app",
+    messagingSenderId: "745256558607",
+    appId: "1:745256558607:web:525d4cd679de607d42037a"
+  }
 };
 
 /*
