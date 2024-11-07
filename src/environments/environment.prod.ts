002081c32fb0f68f6723cd6d58b9b28458c55a19
DaphneCuadra <dap.cuadra@duocuc.cl>
2024-11-07 11:47:22 -0300

    firebase terminado

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyAnNnIZmcy92KCEbGKftAukbKY2PpimpbA",
+    authDomain: "reservapp-3f424.firebaseapp.com",
+    projectId: "reservapp-3f424",
+    storageBucket: "reservapp-3f424.firebasestorage.app",
+    messagingSenderId: "745256558607",
+    appId: "1:745256558607:web:525d4cd679de607d42037a"
+  }
 };
