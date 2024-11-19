6b7ea6eb69909bea56829a8df761b0393e7c3190
alangajardomedina <alan.gajardo@profesor.duoc.cl>
2024-11-19 12:47:49 -0300

    se comienza con pruebas unitarias

--- src/app/pages/home/home.page.spec.ts
+++ src/app/pages/home/home.page.spec.ts
@@ -2,15 +2,41 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
 
 import { HomePage } from './home.page';
+import { RouterTestingModule } from '@angular/router/testing';
 
-describe('HomePage', () => {
+describe('Página Home', () => {
   let component: HomePage;
   let fixture: ComponentFixture<HomePage>;
+  let usuarioPrueba = {
+    "rut": "11111111-1",
+    "nombre": "administrador",
+    "fecha_nacimiento": "1980-01-01",
+    "genero": "Masculino",
+    "correo": "admin@duocuc.cl",
+    "contrasena": "Admin123.",
+    "valida_contrasena": "Admin123.",
+    "tiene_equipo": "no",
+    "nombre_equipo": "",
+    "tipo_usuario": "Administrador"
+  };
 
+  //aqui dentro del beforeEach deben preparar todo lo necesario de la página a testear:
   beforeEach(async () => {
+    const localStoragePrueba = {
+      getItem: jasmine.createSpy('getItem').and.callFake((key: string)=>{
+        if(key==='usuario'){
+          return JSON.stringify(usuarioPrueba);
+        }
+        return null;
+      }),
+      setItem: jasmine.createSpy('setItem'),
+      removeItem: jasmine.createSpy('removeItem')
+    };
+    Object.defineProperty(window,'localStorage', {value: localStoragePrueba});
+    
     await TestBed.configureTestingModule({
       declarations: [HomePage],
-      imports: [IonicModule.forRoot()]
+      imports: [IonicModule.forRoot(), RouterTestingModule]
     }).compileComponents();
 
     fixture = TestBed.createComponent(HomePage);
@@ -18,7 +44,19 @@ describe('HomePage', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  //it: es una prueba unitaria:
+  it('1. Verificar si la página se abre', () => {
+    //aquí dentro va lo que deseo probar:
     expect(component).toBeTruthy();
   });
+
+  it('2. Verificar el nombre del usuario', ()=>{
+    expect(component.usuario.nombre).toEqual("administrador");
+  });
+
+  it('3. Validar el usuario completo', ()=>{
+    expect(localStorage.getItem).toHaveBeenCalledWith('usuario');
+    expect(component.usuario).toEqual(usuarioPrueba);
+  });
+
 });
